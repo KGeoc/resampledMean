5c7332687bab691fb228780f7785fcc84b4ecb80
KGeoc <39421849+KGeoc@users.noreply.github.com>
2019-03-01 18:20:49 -0500

    Update ResampledMean.java
    updated comments and removed excess code

--- ResampledMean.java
+++ ResampledMean.java
@@ -1,8 +1,5 @@
 package resampledMean;
-/*
- * Project goal is to read a file with numbers and, select 100 numbers at random.
- * These 100 random numbers will then be displayed, along with an average for those 100 numbers
- */
+
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -17,20 +14,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Project goal is to read a file with numbers and, select 100 numbers at random.
+ * These 100 random numbers will then be displayed, along with an average for those 100 numbers
+ * @author qstr2
+ *
+ */
 public class ResampledMean {
 
 	public static void main(String[] args) {
-		/*
-		 * 1234 is used to give a predetermined seed, can be removed to allow truly
-		 * random results
-		 */
+
+		// 1234 is used to give a predetermined seed, can be removed to allow truly
+		// random results
+
 		ResampledMean mean = new ResampledMean(1234);
 
 		if (mean.readOriginal("resampledMean/ages.txt")) {
 			System.out.println("File loaded successfully");
 		} else {
-			System.out.println("File not found, creating random array of ages");
-			mean.createRandom(100, 40, 90);
+			System.out.println("File not found");
 		}
 
 		// will create NumFiles files with different values
@@ -44,25 +46,42 @@ public class ResampledMean {
 		}
 	}
 
-	// seed used for creating randomness
+	/**
+	 * seed used for creating randomness
+	 */
 	private Random rand;
-	// holders for original list and lists that will be created
+	/**
+	 * holds original list
+	 */
 	private List<Double> origAges = new ArrayList<Double>();
+	/**
+	 * holds lists that will be created
+	 */
 	private List<Double> newAges = new ArrayList<Double>();
 
+	/**
+	 * generates results using a random seed
+	 */
 	public ResampledMean() {
 		// this will generate results using a random seed
 		rand = new Random();
 	}
 
+	/**
+	 * generates results using a predetermined seed
+	 * 
+	 * @param seed
+	 */
 	public ResampledMean(int seed) {
-		/*
-		 * can set random to a predetermined seed, easier to verify results due to
-		 * consistency
-		 */
+
 		rand = new Random(seed);
 	}
 
+	/**
+	 * calculates mean of generated list
+	 * 
+	 * @return mean of the newAges list
+	 */
 	public double calculateMean() {
 		// calculates the mean of the generated list
 		double total = 0;
@@ -72,10 +91,15 @@ public class ResampledMean {
 		return total / newAges.size();
 	}
 
+	/**
+	 * creates a file using the newly generated list along with the mean
+	 * 
+	 * @param fileNumber used to give each file being generated a different name
+	 * @return returns true if file is successfully created
+	 */
 	public boolean createFile(int fileNumber) {
 		/*
-		 * creates a file using newly generated list fileNumber is what the file will be
-		 * named as to differentiate each result
+		 * 
 		 */
 		try (Writer writer = new BufferedWriter(
 				new OutputStreamWriter(new FileOutputStream(System.getProperty("user.dir")
@@ -103,31 +127,21 @@ public class ResampledMean {
 		return true;
 	}
 
-	public boolean createRandom(int amt, int min, int max) {
-		/*
-		 * will create a random list of amt size from min to max. Used if original list
-		 * is unavailable
-		 */
-		if (min > max) {
-			// checks to make sure range is correct
-			return false;
-		}
-
-		origAges.clear();
-		int range = max - min + 1;
-		for (int i = 0; i < amt; i++) {
-			origAges.add(rand.nextDouble() * range + min);
-		}
-		return true;
-	}
-
+	/**
+	 * returns list of ages using toString method
+	 * 
+	 * @return
+	 */
 	public String displayAges() {
-		// displays age using toString method
 		return newAges.toString();
 	}
 
+	/**
+	 * creates list of ages from original list using amt amount of results
+	 * 
+	 * @param amt
+	 */
 	public void randomAges(int amt) {
-		// creates list of ages from original list using amt amount of results
 		newAges.clear();
 		for (int i = 0; i < amt; i++) {
 			newAges.add(origAges.get(rand.nextInt(origAges.size())));
@@ -135,8 +149,14 @@ public class ResampledMean {
 
 	}
 
+	/**
+	 * reads the original file. weeds out any non entries such as words/letters
+	 * 
+	 * @param fileName name of file with extension
+	 * @return returns true if file read successfully, else false
+	 */
 	public boolean readOriginal(String fileName) {
-		// reads the original file. weeds out any non entries such as words/letters
+		//
 		newAges.clear();
 		BufferedReader fileReader;
 		try {
